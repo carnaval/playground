2e55610798b7828490bbe343d5fb9696323ca617
Oscar Blumberg <oscar.blumberg@ens.fr>
2016-01-31 23:54:34 -0500

    derp

--- x86.jl
+++ x86.jl
@@ -23,6 +23,7 @@ const S_b = 0x2
 const S_z = 0x3
 const S_p = 0x4
 const S_w = 0x5
+const S_d = 0x6
 const F_NEEDS_MODRM = 0x1
 const F_MODRM_EXT = 0x2
 const F_GENERIC = 0x4
@@ -98,6 +99,8 @@ function OpDesc(spec::ASCIIString)
             S_p
         elseif d[1] == 'w'
             S_w
+        elseif d[1] == 'd'
+            S_d
         else
             error(d)
         end
@@ -180,6 +183,9 @@ end
 function op_s!(tab, opc, name, src, flags = 0)
     tab.insn_3ac[opc+1] = InsnDesc3AC(name, OpDesc(), OpDesc(src), OpDesc(), flags)
 end
+function op_d!(tab, opc, name, dst, flags = 0)
+    tab.insn_3ac[opc+1] = InsnDesc3AC(name, OpDesc(dst), OpDesc(), OpDesc(), flags)
+end
 function op_2s!(tab, opc, name, src1, src2, flags = 0)
     tab.insn_3ac[opc+1] = InsnDesc3AC(name, OpDesc(), OpDesc(src1), OpDesc(src2), flags)
 end
@@ -201,9 +207,14 @@ ext_op_3ac!(tab, opc1, opc2, args...) = op_3ac!(tab[opc1], opc2, args...)
 ext_op_2ac!(tab, opc1, opc2, args...) = op_2ac!(tab[opc1], opc2, args...)
 ext_op_d_s!(tab, opc1, opc2, args...) = op_d_s!(tab[opc1], opc2, args...)
 ext_op_s!(tab, opc1, opc2, args...) = op_s!(tab[opc1], opc2, args...)
+ext_op_2s!(tab, opc1, opc2, args...) = op_2s!(tab[opc1], opc2, args...)
+
+const COND_SFX = ["o", "no", "b", "nb", "z", "nz", "be", "nbe",
+                  "s", "ns", "p", "np", "l", "nl", "le", "nle"]
+
 # "standard" instructions
 for (bop, name) in [(0x00, "add"), (0x10, "adc"), (0x20, "and"), (0x30, "xor"),
-                    (0x08, "or"),  (0x18, "sbb"), (0x28, "sub"), (0x38, "cmp")]
+                    (0x08, "or"),  (0x18, "sbb"), (0x28, "sub")]
     op_2ac!(op1_tab, bop+0, name, "Eb", "Gb")
     op_2ac!(op1_tab, bop+1, name, "Ev", "Gv")
     op_2ac!(op1_tab, bop+2, name, "Gb", "Eb")
@@ -215,12 +226,31 @@ op_d_s!(op1_tab, 0x88, "mov", "Eb", "Gb")
 op_d_s!(op1_tab, 0x89, "mov", "Ev", "Gv")
 op_d_s!(op1_tab, 0x8a, "mov", "Gb", "Eb")
 op_d_s!(op1_tab, 0x8b, "mov", "Gv", "Ev")
-# TODO 8c
-op_2ac!(op1_tab, 0x8d, "lea", "Gv", "M")
+op_2s!(op1_tab, 0x38, "cmp", "Eb", "Gb")
+op_2s!(op1_tab, 0x39, "cmp", "Ev", "Gv")
+op_2s!(op1_tab, 0x3a, "cmp", "Gb", "Eb")
+op_2s!(op1_tab, 0x3b, "cmp", "Gv", "Ev")
+op_2s!(op1_tab, 0x3c, "cmp", "CR0b", "Ib")
+op_2s!(op1_tab, 0x3d, "cmp", "CR0v", "Iz")
 
+# TODO 8c
+op_d_s!(op1_tab, 0x8d, "lea", "Gv", "M")
+op_2s!(op1_tab, 0xa8, "test", "CR0b", "Ib")
+op_2s!(op1_tab, 0xa9, "test", "CR0v", "Iz")
+# string operations
+for (op,name) in [(0xa4, "movs"), (0xa6, "cmps"), (0xaa, "stos"), (0xac, "lods"), (0xae, "scas")]
+    # TODO proper operand recording
+    op_0!(op1_tab, op, name)
+    op_0!(op1_tab, op+1, name)
+end
+op_d_s!(op1_tab, 0x63, "movsxd", "Gv", "Ed")
+op_gen!(op1_tab, 0x69, "imul", ("Gv",), ("Ev", "Iz"))
+op_gen!(op1_tab, 0x6b, "imul", ("Gv",), ("Ev", "Ib"))
+op_s!(op1_tab, 0x68, "push", "Iz")
+op_s!(op1_tab, 0x6a, "push", "Ib")
 # jumps
-for (i, name) in enumerate(["jo", "jno", "jb", "jnb", "jz", "jnz", "jbe", "jnbe",
-                            "js", "jns", "jp", "jnp", "jl", "jnl", "jle", "jnle"])
+for (i, name) in enumerate(COND_SFX)
+    name = string("j", name)
     op_s!(op1_tab, 0x70+i-1, name, "Jb")
     op_s!(op2_tab, 0x80+i-1, name, "Jz")
 end
@@ -238,9 +268,12 @@ for act_opc = 0x90:0x90+7
 end
 op_s!(op1_tab, 0xe8, "call", "Jz")
 op_s!(op1_tab, 0xe9, "jmp", "Jz")
-#op_s!(op1_tab, 0xea, "jmp", "Ap")
 op_s!(op1_tab, 0xeb, "jmp", "Jb")
 op_0!(op1_tab, 0xf4, "hlt")
+for (i,name) in enumerate(["clc", "stc", "cli", "sti", "cld", "std"])
+    opc = 0xf8 + i - 1
+    op_0!(op1_tab, opc, name)
+end
 op_s!(op1_tab, 0xc2, "ret", "Iw")
 op_0!(op1_tab, 0xc3, "ret")
 
@@ -250,7 +283,11 @@ for (opc, name) in [(0x84, "test"),]
 end
 op_gen!(op1_tab, 0x86, "xchg", ("Eb", "Gb"), ("Eb", "Gb"))
 op_gen!(op1_tab, 0x86, "xchg", ("Ev", "Gv"), ("Ev", "Gv"))
-
+op_d_s!(op1_tab, 0x98, "cdqe", "CR0v", "CR0b") # CDQE TODO express operands better
+op_0!(op1_tab, 0x99, "cdq") # ditto CDQ
+for op = 0xd8:0xdf
+    op_0!(op1_tab, op, "fp_x87") # TODO add those
+end
 # group 1
 for i = 0:3
     opc = 0x80 + i
@@ -262,7 +299,6 @@ for (j, name) in enumerate(["add", "or", "adc", "sbb", "and", "sub", "xor", "cmp
     j = j-1
     ext_op_3ac!(modrm_ext_tab, 0x80, j, name, o1, "Eb", "Ib")
     ext_op_3ac!(modrm_ext_tab, 0x81, j, name, o2, "Ev", "Iz")
-    #ext_op_3ac!(modrm_ext_tab, 0x82, j, name, o1, "Eb", "Ib") # TODO invalid
     ext_op_3ac!(modrm_ext_tab, 0x83, j, name, o2, "Ev", "Ib")
 end
 # group 2
@@ -278,6 +314,17 @@ for (j, name) in enumerate(["rol", "ror", "rcl", "rcr", "shl", "shr", "shl", "sa
     ext_op_2ac!(modrm_ext_tab,0xd2,j,name, "Eb", "CR1b")
     ext_op_2ac!(modrm_ext_tab,0xd3,j,name, "Ev", "CR1b")
 end
+# group 3
+ext_group!(op1_tab, modrm_ext_tab, 8, 0xf6, "group3")
+ext_group!(op1_tab, modrm_ext_tab, 8, 0xf7, "group3")
+for (i, name) in enumerate(["not", "neg", "mul", "imul", "div", "idiv"])
+    ext_op_d_s!(modrm_ext_tab,0xf6,i+1,name,"Eb","Eb")
+    ext_op_d_s!(modrm_ext_tab,0xf7,i+1,name,"Ev","Ev")
+end
+for i=0:1
+    ext_op_2s!(modrm_ext_tab,0xf6,i,"test","Eb","Ib")
+    ext_op_2s!(modrm_ext_tab,0xf7,i,"test","Ev","Iz")
+end
 # group 5
 ext_group!(op1_tab, modrm_ext_tab, 7, 0xff, "group5")
 for (opc, name, dst) in [(0,"inc", "Ev"), (1, "dec", "Ev"), (6, "push", "")]
@@ -291,10 +338,24 @@ end
 # group 11
 op_d_s!(op1_tab, 0xc6, "mov", "Eb", "Ib")
 op_d_s!(op1_tab, 0xc7, "mov", "Ev", "Iz")
-
-# 2-byte opcodes
-
+####
 op_0!(op2_tab, 0x1f, "nop", F_NEEDS_MODRM) # TODO prefetch is also in there in group 16
+for (i, name) in enumerate(COND_SFX)
+    name = string("cmov", name)
+    op_2ac!(op2_tab, 0x40+i-1, name, "Gv", "Ev")
+end
+for (i, name) in enumerate(COND_SFX)
+    name = string("set", name)
+    op_d!(op2_tab, 0x90+i-1, name, "Eb")
+end
+
+for (opc,name) in [(0xb6, "movzx"), (0xbe, "movsx")]
+    op_d_s!(op2_tab, opc+0, name, "Gv", "Eb")
+    op_d_s!(op2_tab, opc+1, name, "Gv", "Ew")
+end
+op_2s!(op2_tab, 0xa3, "bt", "Ev", "Gv")
+op_2s!(op2_tab, 0xb3, "btr", "Ev", "Gv")
+op_2ac!(op2_tab, 0xaf, "imul", "Gv", "Ev")
 
 immutable Reg
     n :: Int
@@ -317,15 +378,28 @@ Base.show(io::IO, r::IpRel) = @printf(io, "*(ip + %#x)", r.v)
 function Base.show(io::IO, r::Load)
     if r.scale == 0
         if r.disp == 0
+            @assert(r.base !== nothing)
             @printf(io, "*(%s)", r.base)
         else
-            @printf(io, "*(%s + %#x)", r.base, r.disp)
+            if r.base === nothing
+                @printf(io, "*(%#x)", r.disp) # can make sense with a segment prefix
+            else
+                @printf(io, "*(%s + %#x)", r.base, r.disp)
+            end
         end
     else
         if r.disp == 0
-            @printf(io, "*(%s + %d*%s)", r.base, r.scale, r.index)
+            if r.base === nothing
+                @printf(io, "*(%d*%s)", r.scale, r.index)
+            else
+                @printf(io, "*(%s + %d*%s)", r.base, r.scale, r.index)
+            end
         else
-            @printf(io, "*(%s + %d*%s + %#x)", r.base, r.scale, r.index, r.disp)
+            if r.base === nothing
+                @printf(io, "*(%d*%s + %#x)", r.scale, r.index, r.disp)
+            else
+                @printf(io, "*(%s + %d*%s + %#x)", r.base, r.scale, r.index, r.disp)
+            end
         end
     end
 end
@@ -345,16 +419,23 @@ function S_to_typ(s, op_typ)
         error(s)
     end
 end
+
+const PFX_REP = 0xf3
+const PFX_REPN = 0xf2
+
 function go2(io :: IO)
-    pfx1 = 0x0
-    pfx2 = 0x0
-    pfx3 = 0x0
-    pfx4 = 0x0
+    insn_addr = position(io)
+    
+    pfx1 = 0x0 # repeat
+    pfx2 = 0x0 # segment
+    pfx3 = 0x0 # operand size override
+    pfx4 = 0x0 # address size override
+    pfx5 = 0x0 # lock
     pfx_rex = 0x0
     # read prefixes
     while true
         x = read(io, UInt8)
-        if x in (0xf0, 0xf2, 0xf3)
+        if x in (PFX_REP, PFX_REPN)
             pfx1 = x
         elseif x in (0x2e, 0x36, 0x3e, 0x26, 0x64, 0x65)
             pfx2 = x
@@ -362,6 +443,8 @@ function go2(io :: IO)
             pfx3 = x
         elseif x == 0x67
             pfx4 = x
+        elseif x == 0xf0
+            pfx5 = 0xf0
         elseif (x & 0xf0) == 0x40 # TODO only in long mode
             pfx_rex = x
             break
@@ -370,7 +453,7 @@ function go2(io :: IO)
             break
         end
     end
-    @printf("legacy prefix = %#x %#x %#x %#x\n", pfx1, pfx2, pfx3, pfx4)
+    @printf("legacy prefix = %#x %#x %#x %#x %#x\n", pfx1, pfx2, pfx3, pfx4, pfx5)
     if has_rex(pfx_rex)
         @printf("rex prefix = W:%d R:%d X:%d B:%d\n", rex_w(pfx_rex), rex_r(pfx_rex), rex_x(pfx_rex), rex_b(pfx_rex))
     end
@@ -379,11 +462,12 @@ function go2(io :: IO)
 
     # adjust operand & addr size
     op_typ = UInt32
+    if pfx3 == 0x66
+        op_typ = UInt16
+    end
     if has_rex(pfx_rex)
         if rex_w(pfx_rex) != 0
             op_typ = UInt64
-        elseif pfx3 == 0x66
-            op_typ = UInt16
         end
     end
     addr_typ = UInt64
@@ -421,15 +505,30 @@ function go2(io :: IO)
         reg = Reg(modrm_reg_adj)
         rm = nothing
 
-
         if modrm_mod != 0x3
             if modrm_rm == 0x4 # SIB
                 modrm_sib = read(io, UInt8)
+                sib_scale = (modrm_sib & 0xc0) >> 6
+                sib_index = (modrm_sib & 0x38) >> 3
+                sib_base = sib_base_original = modrm_sib & 0x7
+                if has_rex(pfx_rex)
+                    sib_base = sib_base_original | (rex_b(pfx_rex) << 3)
+                    sib_index = sib_index | (rex_x(pfx_rex) << 3)
+                end
+                load_scale = 1 << sib_scale
+                sib_base_reg = Reg(sib_base)
+                if sib_index == 0x4 # sib_index = sp special case
+                    load_scale = 0
+                end
+                if sib_base_original == 0x5 && modrm_mod == 0x0 # sib_base = bp special case
+                    modrm_disp = read(io, UInt32)
+                    sib_base_reg = nothing
+                end
             end
             if modrm_mod == 0x0
                 if modrm_rm == 0x5
                     modrm_disp = read(io, UInt32) # RIP relative addressing
-                    rm = IpRel(disp)
+                    rm = IpRel(modrm_disp)
                 end
             elseif modrm_mod == 0x1
                 modrm_disp = UInt32(read(io, UInt8))
@@ -437,23 +536,8 @@ function go2(io :: IO)
                 modrm_disp = read(io, UInt32)
             end
             if modrm_rm == 0x4 # SIB
-                @show modrm_sib
-                sib_scale = (modrm_sib & 0xc0) >> 6
-                sib_index = (modrm_sib & 0x38) >> 3
-                sib_base = modrm_sib & 0x7
-                @show sib_scale sib_index sib_base
-                # b i s d
-                if has_rex(pfx_rex)
-                    sib_base = sib_base | (rex_b(pfx_rex) << 3) # TODO sib_base == 5 special case
-                    sib_index = sib_index | (rex_x(pfx_rex) << 3)
-                end
-                load_scale = 1 << sib_scale
-                if sib_index == 0x4 # sp special case
-                    load_scale = 0
-                end
-                @show sib_scale sib_index sib_base
-                rm = Load(Reg(sib_base),Reg(sib_index),load_scale,modrm_disp)
-            else
+                rm = Load(sib_base_reg,Reg(sib_index),load_scale,modrm_disp)
+            elseif !(modrm_mod == 0x0 && modrm_rm == 0x5)
                 rm = Load(Reg(modrm_rm_adj),0,0,modrm_disp)
             end
         else
@@ -500,17 +584,17 @@ function go2(io :: IO)
     
     d = map(read_operand, collect(filter(isvalid, dest_ops(desc))))
     s = map(read_operand, collect(filter(isvalid, src_ops(desc))))
-    #=dst = read_operand(desc.dst)
-    src1 = read_operand(desc.src1)
-    src2 = read_operand(desc.src2)=#
+
     dest_s = isempty(d) ? "" : string(join(d, ", "), " <- ")
     src_s = join(s, ", ")
     name = desc.name
+    @printf("%#x  ", insn_addr)
     print_with_color(:green, dest_s, name, " ", src_s, "\n")
 end
 
-function go(s::AbstractString, n = 5)
+function go(s::AbstractString, n = 5, o = 0)
     io = open(s, "r")
+    skip(io, o)
     for i=1:n
         go2(io)
     end
